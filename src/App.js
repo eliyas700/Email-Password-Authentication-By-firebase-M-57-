111387995457794d1295f423012db3258b817330
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-06 22:58:58 +0600

    Create react app,install firebase,solved react warning,create form and some handlers,install react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { getAuth } from "firebase/auth";
+import app from "./firebase.init";
+const auth = getAuth(app);
 function App() {
+  const getEmail = (event) => {
+    console.log(event.target.value);
+  };
+  const getPassword = (event) => {
+    console.log(event.target.value);
+  };
+  const getForm = (event) => {
+    console.log("Form submited");
+    event.preventDefault();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getForm}>
+        <input
+          onBlur={getEmail}
+          type="email"
+          name=""
+          placeholder="Type Your Email"
+          id=""
+        />
+        <br />
+        <input
+          onBlur={getPassword}
+          type="password"
+          name=""
+          id=""
+          placeholder="Type your Password"
+        />
+        <input type="submit" value="Log In" />
+      </form>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHyueVsUSeMX27_KbzEXfL3kF97Ck5JYs",
+  authDomain: "email-password-authentic-5338d.firebaseapp.com",
+  projectId: "email-password-authentic-5338d",
+  storageBucket: "email-password-authentic-5338d.appspot.com",
+  messagingSenderId: "806093267738",
+  appId: "1:806093267738:web:19cd9934fa740ef8c202a3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
