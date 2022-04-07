f79efe2256da75bf3d36890e34d06b6c8faac587
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-07 21:53:17 +0600

    Create User by Using Own Email Address

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
 import "./App.css";
-import { getAuth } from "firebase/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "./firebase.init";
+import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
 const auth = getAuth(app);
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const getEmail = (event) => {
-    console.log(event.target.value);
+    setEmail(event.target.value);
   };
   const getPassword = (event) => {
-    console.log(event.target.value);
+    setPassword(event.target.value);
   };
   const getForm = (event) => {
-    console.log("Form submited");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     event.preventDefault();
   };
   return (
     <div className="App">
-      <form onSubmit={getForm}>
-        <input
-          onBlur={getEmail}
-          type="email"
-          name=""
-          placeholder="Type Your Email"
-          id=""
-        />
-        <br />
-        <input
-          onBlur={getPassword}
-          type="password"
-          name=""
-          id=""
-          placeholder="Type your Password"
-        />
-        <input type="submit" value="Log In" />
-      </form>
+      <div className="w-50 mx-auto">
+        <h2 className="text-primary">Please Register</h2>
+        <Form onSubmit={getForm}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              onBlur={getEmail}
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onBlur={getPassword}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
