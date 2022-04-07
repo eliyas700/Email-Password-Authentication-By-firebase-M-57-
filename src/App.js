2054fb9b50b16a8a81823f3c7a56306785db8373
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-07 23:24:49 +0600

    Change Text according to text

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import { Button, Form } from "react-bootstrap";
 import { useState } from "react";
 const auth = getAuth(app);
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState(false);
 
   const getEmail = (event) => {
     setEmail(event.target.value);
   };
   const getPassword = (event) => {
     setPassword(event.target.value);
+  };
+  console.log(registered);
+  const changeRegister = (event) => {
+    setRegistered(event.target.checked);
   };
   const getForm = (event) => {
     const form = event.currentTarget;
@@ -23,22 +29,34 @@ function App() {
       event.stopPropagation();
       return;
     }
+    if (
+      !/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password)
+    ) {
+      setError("Password Must Contain number,special Character,uppercase");
+      return;
+    }
 
     setValidated(true);
+    setError("");
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
         console.log(user);
+        setEmail(" ");
+        setPassword(" ");
       })
       .catch((error) => {
         console.error(error);
+        setError(error.message);
       });
     event.preventDefault();
   };
   return (
     <div className="App">
       <div className="w-50 mx-auto">
-        <h2 className="text-primary">Please Register</h2>
+        <h2 className="text-primary">
+          Please {registered ? "LogIn" : "Register"}
+        </h2>
         <Form noValidate validated={validated} onSubmit={getForm}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
@@ -48,7 +66,7 @@ function App() {
               placeholder="Enter email"
               required
             />
-            <Form.Control.Feedback type="invalid">
+            <Form.Control.Feedback type="error">
               Please type a valid Email.
             </Form.Control.Feedback>
             <Form.Text className="text-muted">
@@ -64,13 +82,20 @@ function App() {
               placeholder="Password"
               required
             />
-            <Form.Control.Feedback type="invalid">
+            <Form.Control.Feedback type="error">
               Please type a valid password.
             </Form.Control.Feedback>
           </Form.Group>
-
+          <p className="text-danger">{error}</p>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check
+              onChange={changeRegister}
+              type="checkbox"
+              label="Already Registered?"
+            />
+          </Form.Group>
           <Button variant="primary" type="submit">
-            Submit
+            {registered ? "Log In" : "Register"}
           </Button>
         </Form>
       </div>
